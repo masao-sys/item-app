1f1b72b0fcacc865941a082d1460e9344fbaae7e
Takeuchi <masao@MacBook-Ari-Masao.local>
2025-03-01 15:07:18 +0900

    first commit

---
+++ item/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
