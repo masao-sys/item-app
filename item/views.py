d25c7450fd25e161b9e1bc9286f1abbbe1f9c14d
Takeuchi <masao@MacBook-Ari-Masao.local>
2025-03-01 15:49:32 +0900

    商品テーブルを追加

--- item/views.py
+++ item/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import View
+from django.views.generic import View, ListView
 from django.shortcuts import render
+from .models import Item
 
-class HomeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+
+class HomeView(ListView):
+    template_name = 'index.html'
+    model = Item
